18a53ca73df93e4e334588d41e3e0292be6aac71
daehyuk1231 <daehyuk1231@naver.com>
2025-07-18 12:11:51 +0900

    feature/set #2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,14 @@
 module org.example.stayd {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     requires java.sql;
+    requires java.base;
 
+    opens org.example.stayd.domain.user.controller to javafx.fxml;
+    opens org.example.stayd.domain.cafe.controller to javafx.fxml;
+    opens org.example.stayd.domain.reservation.controller to javafx.fxml;
+    opens org.example.stayd.domain.review.controller to javafx.fxml;
 
-    opens org.example.stayd to javafx.fxml;
     exports org.example.stayd;
 }
