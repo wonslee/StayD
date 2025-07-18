9f53039203361d643b1287adc0a595db83aac828
daehyuk1231 <daehyuk1231@naver.com>
2025-07-18 15:22:29 +0900

    feature/user header #5

--- src/main/java/org/example/stayd/domain/user/controller/LoginController.java
+++ src/main/java/org/example/stayd/domain/user/controller/LoginController.java
@@ -1,14 +1,18 @@
 package org.example.stayd.domain.user.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Cursor;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
 import org.example.stayd.common.DatabaseConnection;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -38,8 +42,20 @@ public class LoginController implements Initializable {
     @FXML
     private Label loginMessageLabel;
 
+    @FXML
+    private StackPane headerContainer;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+            // Header.fxml을 로드하여 StackPane에 추가
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/stayd/common/header.fxml"));
+            Parent header = loader.load();
+            headerContainer.getChildren().add(header);
+        } catch (IOException e) {
+            e.printStackTrace();
+            // 예외 발생 시 처리 코드 추가 (예: 사용자에게 에러 메시지 표시)
+        }
 
         setupHoverEffect(findIdLabel);
         setupHoverEffect(findPwLabel);
