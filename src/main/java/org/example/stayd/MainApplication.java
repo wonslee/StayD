85ed0930e667eb11db2dca6aebeeb03d1a7369ce
choiy6 <dudwns1852@naver.com>
2025-07-20 14:01:51 +0900

    feature/스터디카페 생성페이지 1차작업 #3

--- src/main/java/org/example/stayd/MainApplication.java
+++ src/main/java/org/example/stayd/MainApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/org/example/stayd/user/login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/org/example/stayd/cafe/cafeCreateView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
-        stage.setTitle("Hello!");
+        stage.setTitle("cafeCreateView!");
         stage.setScene(scene);
         stage.show();
     }
