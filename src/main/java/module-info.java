d891ee559322f6434417734d05465e04e6c88ff0
choiy6 <dudwns1852@naver.com>
2025-07-22 10:59:29 +0900

    feature/스터디카페 상세 조회 요일 한글로 수정 #10

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,11 +7,17 @@ module org.example.stayd {
     requires jakarta.validation;
     requires jbcrypt;
     requires java.mail;
+    requires jakarta.el; // ← 여기에 추가!!!
+    requires org.hibernate.validator; // 이미 있었겠죠
+    requires com.fasterxml.classmate; // ← 추가 (혹은 requires classmate;)
 
+    opens org.example.stayd.common to javafx.fxml;  // ← 이 줄 추가!
     opens org.example.stayd.domain.user.controller to javafx.fxml;
     opens org.example.stayd.domain.cafe.controller to javafx.fxml;
     opens org.example.stayd.domain.reservation.controller to javafx.fxml;
     opens org.example.stayd.domain.review.controller to javafx.fxml;
 
     exports org.example.stayd;
+
+
 }
